3cc58f38403f59b3e55697359895b816e177d1dc
paulwinex <paulwinex@gmail.com>
2025-07-10 00:04:37 +0300

    added app command class

--- agio_desk/plugins/commands/desk_cmd.py
+++ agio_desk/plugins/commands/desk_cmd.py
@@ -1,20 +1,18 @@
 import logging
-import os
 
-from agio.core.plugins.service_hub import AServiceHub
-from agio.core.plugins.base.command_base import ACommandPlugin
 import click
 
-from agio.core.utils import context
-from agio.core.utils.process_utils import restart_with_env
+from agio.core.plugins.base.command_base import AStartAppCommand
+from agio.core.plugins.service_hub import AServiceHub
 from agio_desk.apps.main import start_desktop_app
 
 logger = logging.getLogger(__name__)
 
 
-class DeskCommand(ACommandPlugin):
+class DeskCommand(AStartAppCommand):
     name = 'desk_cmd'
     command_name = 'desk'
+    app_name = 'desk'
     arguments = [
         click.option("-d", "--headless", is_flag=True, help='Start in headless mode'),
     ]
@@ -24,9 +22,6 @@ class DeskCommand(ACommandPlugin):
     }
 
     def execute(self, **kwargs):
-        if context.app_name != 'desk':
-            logger.info('Restart as application "desk"')
-            restart_with_env({'AGIO_APP_NAME': 'desk'})
         with AServiceHub(self.services) as sh:
             start_desktop_app(headless=kwargs.get('headless', False))
 
