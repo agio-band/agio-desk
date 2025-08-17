677693b5cef8826f6b6cbfe3654242ab09c65514
paulwinex <paulwinex@gmail.com>
2025-08-17 21:17:20 +0300

    refactor DeskService

--- agio_desk/plugins/services/desk_service.py
+++ agio_desk/plugins/services/desk_service.py
@@ -1,5 +1,9 @@
+import logging
+
 from agio.core.events import emit
 from agio.core.plugins.base_service import ServicePlugin, make_action
+
+logger = logging.getLogger(__name__)
 
 
 class DeskService(ServicePlugin):
@@ -7,4 +11,5 @@ class DeskService(ServicePlugin):
 
     @make_action()
     def show_message(self, text: str, title: str = None, *args, **kwargs):
+        logger.info(f'Show message: {text}')
         emit('desk.tray.show_message', {'text': text, 'title': title})
