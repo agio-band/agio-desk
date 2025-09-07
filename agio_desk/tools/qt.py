158ddd1c84ec847a278635523b9b8301e84c4d25
paulwinex <paulwinex@gmail.com>
2025-09-07 15:57:49 +0300

    added open_simple_dialog

--- agio_desk/tools/qt.py
+++ agio_desk/tools/qt.py
@@ -1,7 +1,13 @@
+import logging
+import sys
+
 from PySide6.QtWidgets import QApplication, QMessageBox
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import *
+from PySide6.QtCore import *
 from agio.core.pkg import resources
 
+
+logger = logging.getLogger(__name__)
 
 def get_main_parent():
     qapp = QApplication.instance()
@@ -33,3 +39,22 @@ def message_dialog(title, message, level='info'):
     msg.setIcon(levels.get(level))
     msg.setStandardButtons(QMessageBox.StandardButton.Ok)
     msg.exec_()
+
+
+def open_simple_dialog(widget_class, *args, **kwargs):
+    qapp = QApplication(sys.argv)
+    qapp.setQuitOnLastWindowClosed(True)
+    qapp.setApplicationName(kwargs.pop('app_name', 'agio'))
+
+    # break qt event loop every N time to catch python core events
+    timer = QTimer()
+    timer.start(100)
+    timer.timeout.connect(lambda: None)
+
+    try:
+        w = widget_class(*args, **kwargs)
+        w.show()
+        sys.exit(qapp.exec())
+    except Exception as e:
+        logging.exception("Application startup error")
+
