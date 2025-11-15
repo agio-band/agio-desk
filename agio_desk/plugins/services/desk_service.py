f55dff88c6b52bfd42990adaeb1097e8c41d3f07
paulwinex <paulwinex@gmail.com>
2025-11-15 19:19:54 +0300

    added todo

--- agio_desk/plugins/services/desk_service.py
+++ agio_desk/plugins/services/desk_service.py
@@ -2,6 +2,7 @@ import logging
 
 from agio.core.events import emit
 from agio.core.plugins.base_service import ServicePlugin, make_action
+# from agio.tools import app_context
 
 logger = logging.getLogger(__name__)
 
@@ -12,4 +13,5 @@ class DeskService(ServicePlugin):
     @make_action()
     def show_message(self, text: str, title: str = None, *args, **kwargs):
         logger.info(f'Show message: {text}')
+        # TODO send to desk by localhost
         emit('desk.tray.show_message', {'text': text, 'title': title})
