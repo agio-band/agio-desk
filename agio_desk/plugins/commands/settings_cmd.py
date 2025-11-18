928cfe6fc83c22df5676cf236feedd676d846b11
paulwinex <paulwinex@gmail.com>
2025-11-18 10:06:03 +0300

    added LocalSettingsCommand

---
+++ agio_desk/plugins/commands/settings_cmd.py
@@ -0,0 +1,21 @@
+import logging
+
+from agio.core.plugins.base_command import AStartAppCommand
+from agio.tools.qt import main_app, center_on_screen
+from agio_desk.ui.local_settings_dialog import LocalSettingsDialog
+
+logger = logging.getLogger(__name__)
+
+
+class LocalSettingsCommand(AStartAppCommand):
+    name = 'local_settings_cmd'
+    command_name = 'local-settings'
+    app_name = 'local_settings'
+
+    def execute(self, **kwargs):
+        with main_app() as app:
+            dialog = LocalSettingsDialog()
+            center_on_screen(dialog, app)
+            dialog.show()
+
+
